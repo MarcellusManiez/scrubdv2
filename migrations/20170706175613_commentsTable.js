879a980a78484d5499a9b2c3518d99f66ebc3c3e
bartonyoung <barton.young1986@gmail.com>
2017-07-06 18:15:10 -0700

    Create User/Comment/Video tables.

---
+++ migrations/20170706175613_commentsTable.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('comments', (table) => {
+    table.increments('comment_id').primary();
+    table.string('message');
+    table.integer('user_id').references('user_id').inTable('users');
+  }).then( () => {
+    console.log('comments table created');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('comments');
+};
+
+
