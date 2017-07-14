e857f450ebf153fec6e96ece005a176805434841
bartonyoung <barton.young1986@gmail.com>
2017-07-14 14:37:27 -0700

    Create blank home and login pages.

---
+++ client/containers/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div>
+      HELLO FROM HOME!
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/containers/Login.jsx
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import styles from '../styles/login.css'
+import main from '../styles/main.css'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className={main.container}>
+        <h1>LOGIN MOTHER FUCKER!</h1>
+      </div>
+    )
+  }
+}
+
+export default Login;
+
+        
