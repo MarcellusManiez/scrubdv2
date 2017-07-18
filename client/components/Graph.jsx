33cd80b42ff01587f5f2a60dc8e7e946d1429455
bartonyoung <barton.young1986@gmail.com>
2017-07-18 14:41:14 -0700

    Change styling to graph.

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -4,7 +4,7 @@ import { Sparklines, SparklinesLine, SparklinesCurve, SparklinesBars } from 'rea
 const CommentGraph = ( {data} ) => {
   return (
     <Sparklines data={data} style={{width:'700px', height: '40px', svgHeight: '20px'} }>
-      <SparklinesLine style={{ fill: "red" }} />
+      <SparklinesBars style={{ fill: "#03A9F4" }} />
     </Sparklines>
   )
 };
