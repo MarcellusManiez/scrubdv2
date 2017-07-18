40746ac72048dbf7a96fb6c006cd98b222a1a403
bartonyoung <barton.young1986@gmail.com>
2017-07-17 17:53:31 -0700

    Allow submission on new comment from text field.

--- client/components/CommentBox.jsx
+++ client/components/CommentBox.jsx
@@ -1,45 +1,151 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
+// import RaisedButton from 'material-ui/RaisedButton'
+// import { Menu, MenuItem } from 'material-ui/Menu'
+import { List, ListItem } from 'material-ui/List'
+// import FloatingActionButton from 'material-ui/FloatingActionButton';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {indigo500} from 'material-ui/styles/colors';
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
+import axios from 'axios'
 
 class CommentBox extends Component {
   constructor(props){
     super(props)
-    console.log(this.props)
+
+    this.state = {
+      term : ''
+    };
+
+    this.submitComment = this.submitComment.bind(this)  
+    this.handleTextChange = this.handleTextChange.bind(this)  
+    this.onKeyPress = this.onKeyPress.bind(this)  
   }
+
+
+  submitComment () {
+    const timestamp = this.props.getPlayerTime();
+    const user_name = localStorage.getItem('user');
+    const message = this.state.term;
+    const video_id = this.props.video_id
+    
+    axios.post('/api/addComment', { user_name, timestamp, message, video_id })
+  }
+
+  handleTextChange (e) {
+    e.preventDefault();
+    const term = e.target.value;
+
+    this.setState({ term });
+  }
+
+  onKeyPress(e) {
+  if (e.charCode === 13) { // enter key pressed
+    e.preventDefault();
+    this.submitComment(this.state.term)
+  } 
+}
 
   render () {
     const comment = this.props.currentComment
 
-    if (comment === null || comment === undefined) {
-      return (
-        <div>
-          <TextField
-            hintText='Comment...'
-           />
-        </div>
+    return comment === null ? 
+      (
+        <List>
+          <ListItem 
+            style={styles.list_item}
+            primaryText={
+              <TextField 
+                value={this.state.term} 
+                onKeyPress={this.onKeyPress}
+                onChange={this.handleTextChange}
+                fullWidth={true} 
+                style={styles.textField} 
+                underlineShow={false}/>
+            }
+            rightIcon={
+              <CommunicationChatBubble 
+                onTouchTap={this.submitComment}
+                color={'#03A9F4'}
+                hoverColor={'blue'}
+              />
+            }
+          />
+        </List>
       )
-    } else {
-      return (
-        <div>
-          <TextField />
-          <Card style={{ width: '700px'}}>
-            <CardHeader
-              title={`${comment.user_name} @ ${comment.comment_timestamp}`}
-              subtitle={comment.message}
-              avatar="https://api.adorable.io/avatars/285/Barton Young.png"
-            />
-          </Card>   
-        </div>  
+       :
+      (
+      <div>
+        <List>
+          <ListItem 
+            style={styles.list_item}
+            primaryText={
+              <TextField 
+                value={this.state.term} 
+                onChange={this.handleTextChange}
+                onKeyPress={this.onKeyPress}
+                fullWidth={true} 
+                style={styles.textField} 
+                underlineShow={false}/>
+            }
+            rightIcon={
+              <CommunicationChatBubble 
+                onTouchTap={this.submitComment}
+                color={'#03A9F4'}
+                hoverColor={'blue'}
+              />
+            }
+          />
+        </List>
+        <Card style={{ width: '700px'}}>
+          <CardHeader
+            title={`${comment.user_name} @ ${comment.comment_timestamp}`}
+            subtitle={comment.message}
+            avatar="https://api.adorable.io/avatars/285/Barton Young.png"
+          />
+        </Card>   
+      </div>  
       )
-    } 
   }
 } 
 
 export default CommentBox;
+      
+const styles = {
+
+  
+
+  textField_container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '700px',
+    backgroundColor: 'gray',
+    borderRadius: '2px'
+  },
 
 
-function renderComment( comment ) {
-}
+  list_item : {
+    backgroundColor: '#D3D3D3'
+  },
+
+  textField: {
+    maxHeight: '25px',
+    backgroundColor: 'white',
+    borderRadius: '2px'
+  }
+};
 
 
+{/*<div style={styles.textField_container}>
+        <TextField 
+          value={this.state.term} 
+          inputStyle={styles.textField} 
+          hintText='Comment...'
+          onChange={this.handleTextChange}
+        />
+        <RaisedButton 
+          label='Submit'
+          onTouchTap={this.submitComment}
+        />
+      </div>*/}
