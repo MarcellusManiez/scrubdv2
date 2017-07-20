60b2d3ff820057547071c9c9b0f23f5f452f756e
bartonyoung <barton.young1986@gmail.com>
2017-07-20 11:55:15 -0700

    Minor styling to videoList.

--- client/components/VideoList.jsx
+++ client/components/VideoList.jsx
@@ -6,9 +6,9 @@ import Subheader from 'material-ui/Subheader';
 
 const VideoList = ( { videos, description } ) => {
   return (
-    <div style={styles.root}>
-      <GridList cellHeight={180} style={styles.gridList}>  
-        <Subheader>{description}</Subheader>
+     <div style={styles.root}>
+      <GridList style={styles.gridList}>  
+        <Subheader style={{fontSize:'1.8em', color: 'black'}}>{description}</Subheader>
         { videos.map( vid => 
             <GridTile
               key={vid.video_id}
@@ -36,7 +36,34 @@ const styles = {
   },
   gridList: {
     width: 1000,
-    height: 450,
-    overflowY: 'auto',
+    height: 450
   },
 };
+
+ {/*<div style={styles.root}>
+      <GridList style={styles.gridList}>  
+        <Subheader style={{fontSize:'1.8em', color: 'black'}}>{description}</Subheader>
+        { videos.map( vid => 
+            <GridTile
+              key={vid.video_id}
+              title={vid.video_title}
+              containerElement={<Link to={`/player/${vid.video_id}`}/>}
+            >
+              <img src={vid.thumbnail_url} />
+            </GridTile>
+          ) 
+        }
+      </GridList>
+    </div>*/}
+
+
+
+    {/*<div style={styles.root}>
+      {
+        videos.map( v => 
+          <div style={{display: 'flex', height: 200, justifyContent: 'center'}}>
+            <img src={v.thumbnail_url}/>
+          </div>
+        )
+      }
+    </div>*/}
