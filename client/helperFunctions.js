e8cb704163065f3a685065d6e3cc70d08c145c4a
bartonyoung <barton.young1986@gmail.com>
2017-07-27 15:12:15 -0700

    ESLint Corrections and add Redux to Package JSON.

--- client/helperFunctions.js
+++ client/helperFunctions.js
@@ -1,35 +1,35 @@
 
 module.exports = {
 
-   
-  convertISO8601ToSeconds (input) {
 
+  convertISO8601ToSeconds(input) {
     const reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
-    let hours = 0, minutes = 0, seconds = 0, totalseconds;
+    let hours = 0,
+      minutes = 0,
+      seconds = 0,
+      totalseconds;
 
     if (reptms.test(input)) {
-        var matches = reptms.exec(input);
-        if (matches[1]) hours = Number(matches[1]);
-        if (matches[2]) minutes = Number(matches[2]);
-        if (matches[3]) seconds = Number(matches[3]);
-        totalseconds = hours * 3600  + minutes * 60 + seconds;
+      const matches = reptms.exec(input);
+      if (matches[1]) hours = Number(matches[1]);
+      if (matches[2]) minutes = Number(matches[2]);
+      if (matches[3]) seconds = Number(matches[3]);
+      totalseconds = hours * 3600 + minutes * 60 + seconds;
     }
 
     return (totalseconds);
   },
 
 
-
-
-  getVideoIdFromUrl (string) {
+  getVideoIdFromUrl(string) {
     let id = string.split('v=')[1];
     const ampersandPosition = id.indexOf('&');
-      
-    if(ampersandPosition != -1) {
+
+    if (ampersandPosition != -1) {
       id = id.substring(0, ampersandPosition);
     }
-    
+
     return id;
-  }
+  },
 
 };
