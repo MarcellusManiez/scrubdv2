ffa896f9b6f8ee9674ac1520c77f6c26cc7102e2
bartonyoung <barton.young1986@gmail.com>
2017-07-27 13:40:01 -0700

    Define / export initialState and store.

--- client/Store.js
+++ client/Store.js
@@ -3,6 +3,32 @@ import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import saga from 'redux-saga';
-// import reducer from './reducers/index';
-const middleware = applyMiddleware(thunk, saga);
+// import commentReducer from './reducers/commentReducer';
+import rootReducer from './reducers/index';
 
+const initialState = {
+  videos: {
+    currentVideo: null,
+    userVideos: [],
+    allVideos: [],
+  },
+
+  comments: {
+    videoComments: [],
+    currentComment: [],
+  },
+
+  user: {
+    username: null,
+    userID: null,
+  },
+};
+
+
+const store = createStore(
+  rootReducer,
+  undefined,
+  applyMiddleware(logger));
+
+
+module.exports = { initialState, store };
